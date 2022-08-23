6e9e7d3d815da2df7cfab9c1f4c0cad2574202cc
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-08-22 23:09:48 -0300

    Re ordered folders

--- Assets/SCRIPTS/ControlDireccion.cs
+++ Assets/SCRIPTS/ControlDireccion.cs
@@ -14,56 +14,12 @@ public class ControlDireccion : MonoBehaviour
 	
 	float Giro = 0;
 	
-	public enum Sentido {Der, Izq}
-	Sentido DirAct;
-	
 	public bool Habilitado = true;
 		
 	void Update () 
 	{
 		switch(InputAct)
 		{
-		case TipoInput.Mouse:
-			if(Habilitado) 
-				gameObject.SendMessage("SetGiro", MousePos.Relation(MousePos.AxisRelation.Horizontal));//debe ser reemplanado
-			break;
-			
-		case TipoInput.Kinect:
-
-			if(ManoIzq.position.y > ManoDer.position.y)
-			{
-				DirAct = Sentido.Der;
-			}
-			else
-			{
-				DirAct = Sentido.Izq;
-			}
-			
-			switch(DirAct)
-			{
-			case Sentido.Der:
-				if(Angulo() <= MaxAng)
-					Giro = Angulo() / (MaxAng + DesSencibilidad);
-				else
-					Giro = 1;
-				
-				if(Habilitado)
-					gameObject.SendMessage("SetGiro", Giro);//debe ser reemplanado
-				
-				break;
-				
-			case Sentido.Izq:
-				if(Angulo() <= MaxAng)
-					Giro = (Angulo() / (MaxAng + DesSencibilidad)) * (-1);
-				else
-					Giro = (-1);
-				
-				if(Habilitado)
-					gameObject.SendMessage("SetGiro", Giro);//debe ser reemplanado
-				
-				break;
-			}
-			break;
             case TipoInput.AWSD:
                 if (Habilitado) {
                     if (Input.GetKey(KeyCode.A)) 
@@ -95,13 +51,4 @@ public class ControlDireccion : MonoBehaviour
 	{
 		return Giro;
 	}
-	
-	float Angulo()
-	{
-		Vector2 diferencia = new Vector2(ManoDer.localPosition.x, ManoDer.localPosition.y)
-						   - new Vector2(ManoIzq.localPosition.x, ManoIzq.localPosition.y);
-		
-		return Vector2.Angle(diferencia,new Vector2(1,0));
-	}
-	
 }
